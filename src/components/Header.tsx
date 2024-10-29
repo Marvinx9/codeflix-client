ee71ff725c3558d21c2c1a9e4d12c2e9f541b1cf
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-10-29 19:53:46 -0300

    feat: create navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,23 +2,16 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <header
-            className='
-            fixed
-            top-0
-            z-50
-            flex
-            w-full
-            items-center
-            justify-between
-            px-4
-            py-4
-
-            lg:px-10
-            lg:py-6
-        '
-        >
-            <img src='logo/logo.svg' />
+        <header className='fixed top-0z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10 lg:py-6'>
+            <div className='flex items-center space-x-2 md:space-x-4'>
+                <img src='logo/logo.svg' alt='netflix' width={120} height={120}/>
+                <ul className='hidden md:space-x-4 text-sm font-semibold md:flex'>
+                    <li>Home</li>
+                    <li>TV Shows</li>
+                    <li>Movies</li>
+                    <li>Latest</li>
+                </ul>
+            </div>
         </header>
     )
 }
