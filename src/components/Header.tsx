6dc8e46a2f6616a6b4a6508b34bbf28c623beaa3
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-11-02 10:58:42 -0300

    feat: add useEffect on header for render background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react'
 
 export default function Header() {
+    const [isScrolled, setIsScroled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScroled(true);
+            } else {
+                setIsScroled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []); 
+
     return (
-        <header className='fixed top-0z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10 lg:py-6'>
+    <header className={`${isScrolled && 'bg-black'} fixed top-0z-50 flex w-full items-center justify-between transition-all px-4 py-4 lg:px-10 lg:py-6`}>
             <div className='flex items-center space-x-2 md:space-x-4'>
                 <img src='logo/logo.svg' alt='netflix' width={120} height={120}/>
                 <ul className='hidden md:space-x-4 text-sm font-semibold md:flex'>
